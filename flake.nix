43abd4ec7c35f6c6bd4958735beb435f515f5b56
Ingo Ruhnke <grumbel@gmail.com>
2021-11-10 15:31:35 +0100

    Add Nix flake build file

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A collection of string utilities for C++";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    nix.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    tinycmmc.url = "gitlab:grumbel/cmake-modules";
+    tinycmmc.inputs.nix.follows = "nix";
+    tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
+    tinycmmc.inputs.flake-utils.follows = "flake-utils";
+  };
+
+  outputs = { self, nix, nixpkgs, flake-utils, tinycmmc }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          strutcpp = pkgs.stdenv.mkDerivation {
+            pname = "strutcpp";
+            version = "0.0.0";
+            src = nixpkgs.lib.cleanSource ./.;
+            nativeBuildInputs = [
+              pkgs.cmake
+              pkgs.ninja
+              pkgs.gcc
+            ];
+            buildInputs = [
+              tinycmmc.defaultPackage.${system}
+            ];
+           };
+        };
+        defaultPackage = packages.strutcpp;
+      });
+}
