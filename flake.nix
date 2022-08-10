4b00d49250db4e42e46068d5b4e42a2f8c5ccf51
Ingo Ruhnke <grumbel@gmail.com>
2022-08-10 21:55:44 +0200

    Reorganize flake.nix to allow cross-compiling

--- flake.nix
+++ flake.nix
@@ -3,33 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
-    tinycmmc.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, tinycmmc }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages = flake-utils.lib.flattenTree rec {
-          strutcpp = pkgs.stdenv.mkDerivation {
-            pname = "strutcpp";
-            version = "0.0.0";
-            src = nixpkgs.lib.cleanSource ./.;
-            nativeBuildInputs = [
-              pkgs.cmake
-            ];
-            buildInputs = [
-              tinycmmc.packages.${system}.default
-
-              pkgs.gtest
-            ];
-          };
+  outputs = { self, nixpkgs, tinycmmc }:
+    tinycmmc.lib.eachSystemWithPkgs (pkgs:
+      {
+        packages = rec {
           default = strutcpp;
+          strutcpp = pkgs.callPackage ./strutcpp.nix {
+            tinycmmc = tinycmmc.packages.${pkgs.system}.default;
+          };
         };
       }
     );
