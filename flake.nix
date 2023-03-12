ceac97daf54aea25779efb1f8d1d2b8dbf453528
Ingo Ruhnke <grumbel@gmail.com>
2023-03-12 22:39:47 +0100

    Force a gcc12 build environment for Nix flake
    The default version of GCC in aarch64 is outdated.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A collection of string utilities for C++";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
     tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
@@ -14,6 +14,7 @@
         packages = rec {
           default = strutcpp;
           strutcpp = pkgs.callPackage ./strutcpp.nix {
+            stdenv = pkgs.gcc12Stdenv;
             tinycmmc = tinycmmc.packages.${pkgs.system}.default;
           };
         };
